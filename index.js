c447cf745c91b7d8bda05011d5a26eb87cdfd077
Kyle <kylegranthousel@gmail.com>
2024-02-20 11:54:46 -0500

    Submission for Looping Lab by Kyle Housel

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  //debugger;
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function writeCards(names, event) {
+  const thankYous = [];
+  for (let i = 0; i < names.length; i++) {
+    debugger;
+    let name = names[i];
+    let thankYouMessage = `Thank you, ${name}, for the wonderful ${event} gift!`;
+    thankYous.push(thankYouMessage); 
+  }
+  return thankYous
+}
+
+function countDown(n) {
+  while (n >= 0) {
+    console.log(n)
+    n--
+  }
+}
